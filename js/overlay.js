fa8093d2f388c4a903b104d9dd639f22ffc8584c
marianarvaez11 <maria.narvaez11u@std.uni.edu.ni>
2022-07-03 17:31:29 -0600

    Agregando proyecto

---
+++ js/overlay.js
@@ -0,0 +1,23 @@
+function inicio_sesion_on() {
+    document.getElementById("inicio-sesion").style.display = "block";
+}
+
+function inicio_sesion_off() {
+    document.getElementById("inicio-sesion").style.display = "none";
+}
+
+function overlay_on() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function overlay_off() {
+    document.getElementById("overlay").style.display = "none";
+}
+
+function registro_on() {
+    document.getElementById("registro").style.display = "block";
+}
+
+function registro_off() {
+    document.getElementById("registro").style.display = "none";
+}
